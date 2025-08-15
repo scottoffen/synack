b37d112a5472c1260c163404fbaed41a21431385
Scott Offen <3513626+scottoffen@users.noreply.github.com>
2025-08-15 15:45:46 -0600

    Exclude GuidTraceIdProvider from Test Coverage (#11)

--- src/Synack/Diagnostics/GuidTraceIdentifierProvider.cs
+++ src/Synack/Diagnostics/GuidTraceIdentifierProvider.cs
@@ -1,5 +1,8 @@
+using System.Diagnostics.CodeAnalysis;
+
 namespace Synack.Diagnostics;
 
+[ExcludeFromCodeCoverage]
 public sealed class GuidTraceIdentifierProvider : ITraceIdentifierProvider
 {
     public string GenerateTraceIdentifier()
